73a0f540e5bbdecc9a362390d9890188b617a475
Tanvir Ahmad Arjel <tanvirarjel@outlook.com>
2021-07-06 21:57:35 +0600

    Indentity Compnents have been added for .NET MAUI

--- src/ClientApps/MauiBlazorApp/MauiBlazorApp/Startup.cs
+++ src/ClientApps/MauiBlazorApp/MauiBlazorApp/Startup.cs
@@ -1,9 +1,17 @@
-﻿using MauiBlazorApp.Data;
+﻿using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Reflection;
+using Blazored.LocalStorage;
+using ClientApps.Shared.Common;
+using ClientApps.Shared.Services;
+using MauiBlazorApp.Data;
+using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Components.WebView.Maui;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Maui;
 using Microsoft.Maui.Controls.Hosting;
 using Microsoft.Maui.Hosting;
+using TanvirArjel.Extensions.Microsoft.DependencyInjection;
 
 namespace MauiBlazorApp
 {
@@ -23,6 +31,31 @@ namespace MauiBlazorApp
                 {
                     services.AddBlazorWebView();
                     services.AddSingleton<WeatherForecastService>();
+
+                    services.AddOptions();
+                    services.AddAuthorizationCore();
+                    services.AddScoped<HostAuthStateProvider>();
+                    services.AddScoped<AuthenticationStateProvider>(sp => sp.GetRequiredService<HostAuthStateProvider>());
+
+                    services.AddScoped<JwtSecurityTokenHandler>();
+
+                    services.AddServicesOfAllTypes(typeof(UserService).Assembly);
+                    services.AddServicesOfAllTypes(Assembly.GetExecutingAssembly());
+                    services.AddHttpClient("EmployeeManagementApi", c =>
+                    {
+                        c.BaseAddress = new Uri("https://localhost:44390/api/");
+                        c.DefaultRequestHeaders.Add("Accept", "application/json");
+                        c.DefaultRequestHeaders.Add("User-Agent", "HttpClientFactory-Sample");
+                    }).AddHttpMessageHandler<AuthorizationDelegatingHandler>();
+
+                    services.AddBlazoredLocalStorage(config => config.JsonSerializerOptions.WriteIndented = true);
+
+                    services.AddHttpClient("IdentityApi", c =>
+                    {
+                        c.BaseAddress = new Uri("https://localhost:44363/api/");
+                        c.DefaultRequestHeaders.Add("Accept", "application/json");
+                        c.DefaultRequestHeaders.Add("User-Agent", "HttpClientFactory-Sample");
+                    }).AddHttpMessageHandler<AuthorizationDelegatingHandler>();
                 });
         }
     }
