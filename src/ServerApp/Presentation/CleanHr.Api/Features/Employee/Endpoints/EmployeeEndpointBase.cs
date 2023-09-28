4980a2c73b487b24045e883f3aec9f995649fb1e
Tanvir Arjel <tanvir.arjel@agoda.com>
2023-09-28 19:04:05 +0700

    Employee Feature folder added.

---
+++ src/ServerApp/Presentation/CleanHr.Api/Features/Employee/Endpoints/EmployeeEndpointBase.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CleanHr.Api.Features.Employee.Endpoints;
+
+[ApiController]
+[ApiVersion("1.0")]
+[Route("api/v{version:apiVersion}/employees")]
+[Authorize]
+[ApiExplorerSettings(GroupName = "Employee Endpoints")]
+public abstract class EmployeeEndpointBase : ControllerBase
+{
+}
