7da210a8ff3078c62e0b2702ed50f4adac6be929
Tanvir Ahmad Arjel <ta.arjel@gmail.com>
2023-10-07 11:03:23 +0700

    Validation updated. (#21)
    * External login feature endpoint added.
    
    * Validation updated.
    
    ---------
    
    Co-authored-by: Tanvir Arjel <tanvir.arjel@agoda.com>

--- src/ServerApp/Presentation/CleanHr.Api/Features/Department/Validators/CreateDepartmentModelValidator.cs
+++ src/ServerApp/Presentation/CleanHr.Api/Features/Department/Validators/CreateDepartmentModelValidator.cs
@@ -1,10 +1,17 @@
 ﻿using CleanHr.Api.Features.Department.Models;
+using FluentValidation;
+using MediatR;
 
 namespace CleanHr.Api.Features.Department.Validators;
 
 public sealed class CreateDepartmentModelValidator : DepartmentBaseModelValidator<CreateDepartmentModel>
 {
-    public CreateDepartmentModelValidator()
+    public CreateDepartmentModelValidator(IMediator mediator)
+        : base(mediator)
     {
+        RuleFor(d => d.Name)
+               .ValidName()
+               .MustAsync(async (model, name, token) => await IsUniqueNameAsync(Guid.Empty, name, token))
+               .WithMessage("The DepartmentName is already existent.");
     }
 }
---
+++ src/ServerApp/Presentation/CleanHr.Api/Features/Department/Validators/DepartmentRuleBuilderExtensions.cs
@@ -0,0 +1,19 @@
+﻿using CleanHr.Api.Features.Department.Models;
+using FluentValidation;
+
+namespace CleanHr.Api;
+
+public static class DepartmentRuleBuilderExtensions
+{
+    public static IRuleBuilderOptions<T, string> ValidName<T>(
+        this IRuleBuilder<T, string> rule)
+        where T : DepartmentBaseModel
+    {
+        return rule.NotEmpty()
+           .WithMessage("The {PropertyName} is required.")
+           .MinimumLength(2)
+           .WithMessage("The {PropertyName} must be at least {MinLength} characters.")
+           .MaximumLength(20)
+           .WithMessage("The {PropertyName} can't be more than {MaxLength} characters.");
+    }
+}
